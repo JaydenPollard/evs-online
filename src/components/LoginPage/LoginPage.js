03a58cb803fa789b2c8026529bf4a60072304caf
Minh Vu <13066461@student.uts.edu.au>
2019-04-15 10:02:32 +1000

    EVS: Modified styling for login page

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -12,21 +12,31 @@ import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import withStyles from "@material-ui/core/styles/withStyles";
+import Image from "../../assests/popcorn-login-background.jpg";
+import Divider from "@material-ui/core/Divider";
 
 const styles = theme => ({
+	background: {
+		width: "100vw",
+		height: "100vh",
+		backgroundImage: `url(${Image})`,
+		backgroundPosition: "center",
+		backgroundRepeat: "no-repeat",
+		backgroundSize: "cover",
+		position: "relative"
+	},
 	main: {
-		width: "auto",
+		width: "100%",
 		display: "block", // Fix IE 11 issue.
-		marginLeft: theme.spacing.unit * 3,
-		marginRight: theme.spacing.unit * 3,
+		marginLeft: "auto",
+		marginRight: "auto",
 		[theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
-			width: 400,
-			marginLeft: "auto",
-			marginRight: "auto"
-		}
+			width: 400
+		},
+		paddingTop: "10vh",
+		top: "10%"
 	},
 	paper: {
-		marginTop: theme.spacing.unit * 8,
 		display: "flex",
 		flexDirection: "column",
 		alignItems: "center",
@@ -39,10 +49,17 @@ const styles = theme => ({
 	},
 	form: {
 		width: "100%", // Fix IE 11 issue.
-		marginTop: theme.spacing.unit
+		marginTop: theme.spacing.unit,
+		marginBottom: theme.spacing.unit
 	},
 	submit: {
 		marginTop: theme.spacing.unit * 3
+	},
+	divider: {
+		height: theme.spacing.unit/5,
+		backgroundColor: "#000",
+		width: "80%",
+		marginTop: theme.spacing.unit*2,
 	}
 });
 
@@ -50,45 +67,50 @@ function LogIn(props) {
 	const { classes } = props;
 
 	return (
-		<main className={classes.main}>
-			<CssBaseline />
-			<Paper className={classes.paper}>
-				<Avatar className={classes.avatar}>
-					<LockOutlinedIcon />
-				</Avatar>
-				<Typography component="h1" variant="h5">
-					Log in
-				</Typography>
-				<form className={classes.form}>
-					<FormControl margin="normal" required fullWidth>
-						<InputLabel htmlFor="email">Email Address</InputLabel>
-						<Input id="email" name="email" autoComplete="email" autoFocus />
-					</FormControl>
-					<FormControl margin="normal" required fullWidth>
-						<InputLabel htmlFor="password">Password</InputLabel>
-						<Input
-							name="password"
-							type="password"
-							id="password"
-							autoComplete="current-password"
-						/>
-					</FormControl>
-					<FormControlLabel
-						control={<Checkbox value="remember" color="primary" />}
-						label="Remember me"
-					/>
-					<Button
-						type="submit"
-						fullWidth
-						variant="contained"
-						color="primary"
-						className={classes.submit}
-					>
+		<div className={classes.background}>
+			<main className={classes.main}>
+				<CssBaseline />
+				<Paper className={classes.paper}>
+					<Avatar className={classes.avatar}>
+						<LockOutlinedIcon />
+					</Avatar>
+					<Typography component="h1" variant="h5">
 						Log in
-					</Button>
-				</form>
-			</Paper>
-		</main>
+					</Typography>
+					<form className={classes.form}>
+						<FormControl margin="normal" required fullWidth>
+							<InputLabel htmlFor="email">Email Address</InputLabel>
+							<Input id="email" name="email" autoComplete="email" autoFocus />
+						</FormControl>
+						<FormControl margin="normal" required fullWidth>
+							<InputLabel htmlFor="password">Password</InputLabel>
+							<Input
+								name="password"
+								type="password"
+								id="password"
+								autoComplete="current-password"
+							/>
+						</FormControl>
+						<FormControlLabel
+							control={<Checkbox value="remember" color="primary" />}
+							label="Remember me"
+						/>
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							color="primary"
+							className={classes.submit}
+						>
+							Log in
+						</Button>
+					</form>
+					<div className={classes.divider} />
+					<form className={classes.form}>
+					</form>
+				</Paper>
+			</main>
+		</div>
 	);
 }
 
