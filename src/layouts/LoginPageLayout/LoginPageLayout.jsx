9e449f3dcd5a517b46f8af999c73b983ab2e6566
dquangt <tomkieu2505@gmail.com>
2019-05-15 10:50:02 +1000

    Adding login page

--- src/layouts/LoginPageLayout/LoginPageLayout.jsx
+++ src/layouts/LoginPageLayout/LoginPageLayout.jsx
@@ -1,93 +1,139 @@
-import ArrowBackIcon from "@material-ui/icons/ArrowBackRounded";
 import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import Checkbox from "@material-ui/core/Checkbox";
-import CssBaseline from "@material-ui/core/CssBaseline";
+import React, { Component } from "react";
+import { validate } from "./validate";
+import FormField from "./formFields";
 import FormControl from "@material-ui/core/FormControl";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Input from "@material-ui/core/Input";
-import InputLabel from "@material-ui/core/InputLabel";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Paper from "@material-ui/core/Paper";
-import PropTypes from "prop-types";
-import React from "react";
-import Typography from "@material-ui/core/Typography";
 import withStyles from "@material-ui/core/styles/withStyles";
-import { Link } from "react-router-dom";
+import Typography from "@material-ui/core/Typography";
 import { loginPageLayoutStyles } from "./LoginPageLayoutStyles";
+import Image from "../../assests/popcorn-login-background.jpg";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Paper from "@material-ui/core/Paper";
+// import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+// import {firebase} from '../firebase';
+class LoginPage extends Component {
+    constructor(props) {
+        super(props);
+        const { classes } = props;
+    }
+    state = {
+        formError: false,
+        formSuccess: "",
+        formdata: {
+            email: {
+                element: "input",
+                value: "",
+                config: {
+                    name: "email_input",
+                    label: "Email address",
+                    type: "email",
+                    placeholder: "Enter your email address: *"
+                },
+                validation: {
+                    required: true,
+                    email: true
+                },
+                valid: false,
+                validationMessage: "",
+                showlabel: true
+            },
+            password: {
+                element: "input",
+                value: "",
+                config: {
+                    name: "password_input",
+                    type: "password",
+                    label: "Password",
+                    placeholder: "Enter your password: *"
+                },
+                validation: {
+                    required: true
+                },
+                valid: false,
+                validationMessage: "",
+                showlabel: true
+            }
+        }
+    };
+    updateForm(element) {
+        const newFormdata = { ...this.state.formdata };
+        const newElement = { ...newFormdata[element.id] };
 
-function LoginPageLayout(props) {
-    const { classes } = props;
+        newElement.value = element.event.target.value;
 
-    return (
-        <div className={classes.background}>
-            <main className={classes.main}>
-                <CssBaseline />
-                <Paper className={classes.paper}>
-                    <Button className={classes.button} color="default">
-                        <Link to="/home" style={{ textDecoration: "none" }}>
-                            <ArrowBackIcon
-                                style={{ transform: "translateY(7px)" }}
-                            />
-                            Return
-                        </Link>
-                    </Button>
+        let validData = validate(newElement);
+        newElement.valid = validData[0];
+        newElement.validationMessage = validData[1];
 
-                    <Avatar className={classes.avatar}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Log in
-                    </Typography>
-                    <form className={classes.form}>
-                        <FormControl margin="normal" required fullWidth>
-                            <InputLabel htmlFor="email">
-                                Email Address
-                            </InputLabel>
-                            <Input
-                                id="email"
-                                name="email"
-                                autoComplete="email"
-                                autoFocus
-                            />
-                        </FormControl>
-                        <FormControl margin="normal" required fullWidth>
-                            <InputLabel htmlFor="password">Password</InputLabel>
-                            <Input
-                                name="password"
-                                type="password"
-                                id="password"
-                                autoComplete="current-password"
-                            />
-                        </FormControl>
-                        <FormControlLabel
-                            control={
-                                <Checkbox value="remember" color="primary" />
-                            }
-                            label="Remember me"
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="inherit"
-                            className={classes.submit}
+        newFormdata[element.id] = newElement;
+
+        this.setState({
+            formError: false,
+            formdata: newFormdata
+        });
+    }
+    submitForm(event) {
+        event.preventDefault();
+
+        let dataToSubmit = {};
+        let formIsValid = true;
+
+        for (let key in this.state.formdata) {
+            dataToSubmit[key] = this.state.formdata[key].value;
+            formIsValid = this.state.formdata[key].valid && formIsValid;
+        }
+    }
+
+    render() {
+        return (
+            <div className={this.props.classes.background}>
+                <main className={this.props.classes.main}>
+                    <CssBaseline />
+                    <Paper className={this.props.classes.paper}>
+                        <Avatar className={this.props.classes.avatar} />
+                        <Typography component="h1" variant="h5">
+                            {" "}
+                            Log in{" "}
+                        </Typography>
+                        <form
+                            className={this.props.classes.form}
+                            onSubmit={event => this.submitForm(event)}
                         >
-                            Log in
-                        </Button>
-                    </form>
-                    {/* <div className={classes.divider} /> */}
-                    <form className={classes.form}>
-                        {/* TODO: Add Google and Facebook Login Button here */}
-                    </form>
-                </Paper>
-            </main>
-        </div>
-    );
+                            <FormControl margin="normal" required fullWidth>
+                                <FormField
+                                    id={"email"}
+                                    formdata={this.state.formdata.email}
+                                    change={element => this.updateForm(element)}
+                                />
+                            </FormControl>
+                            <FormControl margin="normal" required fullWidth>
+                                <FormField
+                                    id={"password"}
+                                    formdata={this.state.formdata.password}
+                                    change={element => this.updateForm(element)}
+                                />
+                            </FormControl>
+                            {this.state.formError ? (
+                                <div className="error_label">
+                                    Something is wrong, try again
+                                </div>
+                            ) : null}
+                            <button
+                                onClick={event => this.submitForm(event)}
+                                fullWidth
+                                variant="contained"
+                                color="inherit"
+                                className={this.props.classes.submit}
+                            >
+                                Log In
+                            </button>
+                            <div> {this.state.formSuccess} </div>
+                        </form>
+                    </Paper>
+                </main>
+            </div>
+        );
+    }
 }
 
-LoginPageLayout.propTypes = {
-    classes: PropTypes.object.isRequired
-};
-
-export default withStyles(loginPageLayoutStyles)(LoginPageLayout);
+export default withStyles(loginPageLayoutStyles)(LoginPage);
