e49f1aff1fcd0a494494fc695601fafc518808de
bazerama <sebvaughan97@gmail.com>
2019-05-15 10:11:23 +1000

    feat: firebase config and init

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,19 @@ import { Route, BrowserRouter } from "react-router-dom";
 import { Switch } from "react-router";
 import HomePage from "./layouts/HomePageLayout/HomePageLayout";
 import LoginPage from "./layouts/LoginPageLayout/LoginPageLayout";
+import firebase from "firebase";
+
+//initialize firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyCTT8GvpjD5AMOMkAQZlP8Pj8P57n70jJg",
+    authDomain: "tba-evs.firebaseapp.com",
+    databaseURL: "https://tba-evs.firebaseio.com",
+    projectId: "tba-evs",
+    storageBucket: "tba-evs.appspot.com",
+    messagingSenderId: "747412141354",
+    appId: "1:747412141354:web:59d4cf6f365fc635"
+};
+firebase.initializeApp(firebaseConfig);
 
 const App = () => (
     <BrowserRouter>
