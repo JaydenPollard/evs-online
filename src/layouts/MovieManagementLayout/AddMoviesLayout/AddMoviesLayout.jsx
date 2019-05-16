0bc117b018aca0fc578c6b49384aaf3d51e92603
Joshua Wu <26704772+apksdk@users.noreply.github.com>
2019-05-16 20:48:26 +1000

    fix: fix alignment issue for add movies

--- src/layouts/MovieManagementLayout/AddMoviesLayout/AddMoviesLayout.jsx
+++ src/layouts/MovieManagementLayout/AddMoviesLayout/AddMoviesLayout.jsx
@@ -11,8 +11,9 @@ import AppBar from "../../../components/AppBar/AppBar";
 import stockMovieImage from "../../../assests/stockMovieImage.jpg";
 
 const imagePreviewStyle = {
-    height: "200px",
-    width: "140px"
+    height: "336px",
+    width: "200px",
+    marginRight: "16px"
 };
 
 const AddMoviesLayout = () => {
@@ -50,74 +51,76 @@ const AddMoviesLayout = () => {
                             title="Uploaded Movie Image Preview"
                             image={stockMovieImage}
                         />
-                        <Grid
-                            container
-                            direction="column"
-                            justify="center"
-                            alignItems="center"
-                            spacing={8}
-                        >
-                            <Grid item>
-                                <TextField
-                                    required
-                                    id="movieName"
-                                    name="movieName"
-                                    label="Movie Name"
-                                    fullWidth
-                                    autoComplete="movieName"
-                                />
+                        <Grid item>
+                            <Grid
+                                container
+                                direction="column"
+                                justify="center"
+                                alignItems="center"
+                                spacing={8}
+                            >
+                                <Grid item>
+                                    <TextField
+                                        required
+                                        id="movieName"
+                                        name="movieName"
+                                        label="Movie Name"
+                                        fullWidth
+                                        autoComplete="movieName"
+                                    />
+                                </Grid>
+                                <Grid item>
+                                    <TextField
+                                        required
+                                        id="movieRating"
+                                        name="movieRating"
+                                        label="Movie Rating"
+                                        fullWidth
+                                        autoComplete="movieRating"
+                                    />
+                                </Grid>
+                                <Grid item>
+                                    <TextField
+                                        required
+                                        id="movieLength"
+                                        name="movieLength"
+                                        label="Movie Length (min)"
+                                        fullWidth
+                                        autoComplete="movieLength"
+                                    />
+                                </Grid>
+                                <Grid item>
+                                    <TextField
+                                        required
+                                        id="movieGenre"
+                                        name="movieGenre"
+                                        label="Movie Genre"
+                                        fullWidth
+                                        autoComplete="movieGenre"
+                                    />
+                                </Grid>
+                                <Grid item>
+                                    <TextField
+                                        required
+                                        id="moviePrice"
+                                        name="moviePrice"
+                                        label="Movie Price"
+                                        fullWidth
+                                        autoComplete="moviePrice"
+                                    />
+                                </Grid>
+                                <Grid item>
+                                    <TextField
+                                        required
+                                        id="movieReleaseDate"
+                                        name="movieReleaseDate"
+                                        label="Movie Release Date"
+                                        fullWidth
+                                        autoComplete="movieReleaseDate"
+                                    />
+                                </Grid>
+                                <Button>Add Movie</Button>
                             </Grid>
-                            <Grid item>
-                                <TextField
-                                    required
-                                    id="movieRating"
-                                    name="movieRating"
-                                    label="Movie Rating"
-                                    fullWidth
-                                    autoComplete="movieRating"
-                                />
-                            </Grid>
-                            <Grid item>
-                                <TextField
-                                    required
-                                    id="movieLength"
-                                    name="movieLength"
-                                    label="Movie Length (min)"
-                                    fullWidth
-                                    autoComplete="movieLength"
-                                />
-                            </Grid>
-                            <Grid item>
-                                <TextField
-                                    required
-                                    id="movieGenre"
-                                    name="movieGenre"
-                                    label="Movie Genre"
-                                    fullWidth
-                                    autoComplete="movieGenre"
-                                />
-                            </Grid>
-                            <Grid item>
-                                <TextField
-                                    required
-                                    id="moviePrice"
-                                    name="moviePrice"
-                                    label="Movie Price"
-                                    fullWidth
-                                    autoComplete="moviePrice"
-                                />
-                            </Grid>
-                            <Grid item>
-                                <TextField
-                                    required
-                                    id="movieReleaseDate"
-                                    name="movieReleaseDate"
-                                    label="Movie Release Date"
-                                    fullWidth
-                                    autoComplete="movieReleaseDate"
-                                />
-                            </Grid>
-                            <Button>Add Movie</Button>
                         </Grid>
                     </Grid>
                 </Grid>
