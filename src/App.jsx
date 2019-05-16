08a5ee2574bc876d3f59eb7cff02c5b91abc1191
Joshua Wu <26704772+apksdk@users.noreply.github.com>
2019-05-16 20:52:32 +1000

    feat: implement dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 import { Route, BrowserRouter } from "react-router-dom";
 import { Switch } from "react-router";
+import {
+    MuiThemeProvider,
+    createMuiTheme,
+    CssBaseline
+} from "@material-ui/core";
 import HomePage from "./layouts/HomePageLayout/HomePageLayout";
 import LoginPage from "./layouts/LoginPageLayout/LoginPageLayout";
 
+const theme = createMuiTheme({
+    palette: {
+        type: "dark"
+    }
+});
+
 const App = () => (
-    <BrowserRouter>
-        <Switch>
-            <Route path="/home" exact component={HomePage} />
-            <Route path="/login" exact component={LoginPage} />
-            <Route component={HomePage} />
-        </Switch>
-    </BrowserRouter>
+    <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+            <Switch>
+                <Route path="/home" exact component={HomePage} />
+                <Route path="/login" exact component={LoginPage} />
+                <Route component={HomePage} />
+            </Switch>
+        </BrowserRouter>
+    </MuiThemeProvider>
 );
 export default App;
