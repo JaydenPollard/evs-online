5c861ff8e8de709f7127dba275af4a3f19289061
bazerama <Sebastian.Vaughan@student.uts.edu.au>
2019-05-20 22:50:57 +1000

    fix: firebase with react context api

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import Firebase, { FirebaseContext } from "./components/Firebase";
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <FirebaseContext.Provider value={new Firebase()}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </FirebaseContext.Provider>,
     document.getElementById("root")
 );
