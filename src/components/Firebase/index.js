335265e8e31c2a0a551c75cdf18128e7c6f08ff6
Joshua Wu <26704772+apksdk@users.noreply.github.com>
2019-05-24 17:21:50 +1000

    fix: firebase initialization

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,6 +1,6 @@
 import FirebaseContext from "./FirebaseContext";
 import Firebase from "./Firebase";
 
+// Export both components here for cleaner import statements
 export default Firebase;
-
 export { FirebaseContext };
