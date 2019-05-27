79c5faa772c72d3c31512767b2ba4ee3994744a1
Joshua Wu <26704772+apksdk@users.noreply.github.com>
2019-05-27 18:08:57 +1000

    wip: fix import

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import {
     CssBaseline
 } from "@material-ui/core";
 import LoginPage from "./layouts/LoginPageLayout/LoginPageLayout";
-import MovieManagementPage from "./layouts/MovieLayout/ViewMoviesLayout/ViewMoviesLayout";
+import ViewMoviesLayout from "./layouts/MovieLayout/ViewMoviesLayout/ViewMoviesLayout";
 import AddMoviesLayout from "./layouts/MovieLayout/MovieContentProcessingLayout/MovieContentProcessingLayout";
 
 const theme = createMuiTheme({
@@ -21,14 +21,14 @@ const App = () => (
         <CssBaseline />
         <BrowserRouter>
             <Switch>
-                <Route path="/home" exact component={MovieManagementPage} />
+                <Route path="/home" exact component={ViewMoviesLayout} />
                 <Route path="/login" exact component={LoginPage} />
                 <Route
                     path="/management/movie"
                     exact
                     component={AddMoviesLayout}
                 />
-                <Route component={MovieManagementPage} />
+                <Route component={ViewMoviesLayout} />
             </Switch>
         </BrowserRouter>
     </MuiThemeProvider>
