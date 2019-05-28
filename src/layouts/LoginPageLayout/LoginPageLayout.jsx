f3026dd7583dee10f1eefa11c5ec2bd6407a1aa7
dquangt <tomkieu2505@gmail.com>
2019-05-28 19:54:01 +1000

    Recode login page

--- src/layouts/LoginPageLayout/LoginPageLayout.jsx
+++ src/layouts/LoginPageLayout/LoginPageLayout.jsx
@@ -1,139 +1,87 @@
-import Avatar from "@material-ui/core/Avatar";
-import React, { Component } from "react";
-import { validate } from "./validate";
-import FormField from "./formFields";
-import FormControl from "@material-ui/core/FormControl";
+
+import React, { useState } from "react";
+
 import withStyles from "@material-ui/core/styles/withStyles";
-import Typography from "@material-ui/core/Typography";
-import { loginPageLayoutStyles } from "./LoginPageLayoutStyles";
-import Image from "../../assests/popcorn-login-background.jpg";
+import { Typography, Paper, Avatar, Button, FormControl, Input, InputLabel } from '@material-ui/core'
+import moment from 'moment';
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Paper from "@material-ui/core/Paper";
+// import Image from "../../assests/popcorn-login-background.jpg";
+
+
+import { validate } from "./validate";
+import { loginPageLayoutStyles } from "./LoginPageLayoutStyles";
 // import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-// import {firebase} from '../firebase';
-class LoginPage extends Component {
-    constructor(props) {
-        super(props);
-        const { classes } = props;
+import { firebase} from "../firebase";
+
+function LoginPage(props) {
+    const {classes} = props;
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [err, setErr] = useState('')
+
+    const handleEmail = e => {
+        setEmail(e.target.value);
     }
-    state = {
-        formError: false,
-        formSuccess: "",
-        formdata: {
-            email: {
-                element: "input",
-                value: "",
-                config: {
-                    name: "email_input",
-                    label: "Email address",
-                    type: "email",
-                    placeholder: "Enter your email address: *"
-                },
-                validation: {
-                    required: true,
-                    email: true
-                },
-                valid: false,
-                validationMessage: "",
-                showlabel: true
-            },
-            password: {
-                element: "input",
-                value: "",
-                config: {
-                    name: "password_input",
-                    type: "password",
-                    label: "Password",
-                    placeholder: "Enter your password: *"
-                },
-                validation: {
-                    required: true
-                },
-                valid: false,
-                validationMessage: "",
-                showlabel: true
-            }
-        }
-    };
-    updateForm(element) {
-        const newFormdata = { ...this.state.formdata };
-        const newElement = { ...newFormdata[element.id] };
-
-        newElement.value = element.event.target.value;
-
-        let validData = validate(newElement);
-        newElement.valid = validData[0];
-        newElement.validationMessage = validData[1];
-
-        newFormdata[element.id] = newElement;
-
-        this.setState({
-            formError: false,
-            formdata: newFormdata
-        });
-    }
-    submitForm(event) {
-        event.preventDefault();
-
-        let dataToSubmit = {};
-        let formIsValid = true;
-
-        for (let key in this.state.formdata) {
-            dataToSubmit[key] = this.state.formdata[key].value;
-            formIsValid = this.state.formdata[key].valid && formIsValid;
-        }
-    }
-
-    render() {
+    function login() {
+		
+        firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then(() => {
+          
+           props.history.push('/accesslog');
+           let dbref = firebase.database().ref("AccessLog/");
+           var newDbRef = dbref.push();
+           newDbRef.set({
+               date: moment(Date()).format("DD/MM/YYYY"),
+               time: moment(Date()).format("hh:mm A"),
+               userID: "C001"
+           })
+           setErr("Succesful");
+           
+        })
+        .catch(error => {
+         setErr(error.message);
+		})
+	}
+    
         return (
-            <div className={this.props.classes.background}>
-                <main className={this.props.classes.main}>
+            <div className={classes.background}>
+                <main className={classes.main}>
                     <CssBaseline />
-                    <Paper className={this.props.classes.paper}>
-                        <Avatar className={this.props.classes.avatar} />
+                    <Paper className={classes.paper}>
+                        <Avatar className={classes.avatar} />
                         <Typography component="h1" variant="h5">
-                            {" "}
-                            Log in{" "}
+                            
+                            Log in
                         </Typography>
-                        <form
-                            className={this.props.classes.form}
-                            onSubmit={event => this.submitForm(event)}
-                        >
+                        <form className={classes.form} onSubmit={e => e.preventDefault() && false}>
                             <FormControl margin="normal" required fullWidth>
-                                <FormField
-                                    id={"email"}
-                                    formdata={this.state.formdata.email}
-                                    change={element => this.updateForm(element)}
-                                />
+                                <InputLabel htmlFor="email">Email Address</InputLabel>
+                                <Input id="email" name="email" autoComplete="off" autoFocus value={email} onChange={e => handleEmail(e)} />
                             </FormControl>
                             <FormControl margin="normal" required fullWidth>
-                                <FormField
-                                    id={"password"}
-                                    formdata={this.state.formdata.password}
-                                    change={element => this.updateForm(element)}
-                                />
+                                <InputLabel htmlFor="password">Password</InputLabel>
+                                <Input name="password" type="password" id="password" autoComplete="off" value={password} onChange={e => setPassword(e.target.value)} />
                             </FormControl>
-                            {this.state.formError ? (
-                                <div className="error_label">
-                                    Something is wrong, try again
-                                </div>
-                            ) : null}
-                            <button
-                                onClick={event => this.submitForm(event)}
+                            <Button
+                                type="submit"
                                 fullWidth
                                 variant="contained"
-                                color="inherit"
-                                className={this.props.classes.submit}
+                                color="primary"
+                                onClick={login}
+                                className={classes.submit}
                             >
-                                Log In
-                            </button>
-                            <div> {this.state.formSuccess} </div>
+						    Log in
+          			        </Button>
+                    
+                            <div> {err} </div>
                         </form>
                     </Paper>
                 </main>
             </div>
         );
-    }
+    
 }
 
 export default withStyles(loginPageLayoutStyles)(LoginPage);
